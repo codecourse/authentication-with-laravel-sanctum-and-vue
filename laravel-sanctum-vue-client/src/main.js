426fbe3620509b4b71a9a371df269d06944374e8
Alex Garrett-Smith <alex@codecourse.com>
2024-05-15 15:02:29 +0100

    Episode 10

--- laravel-sanctum-vue-client/src/main.js
+++ laravel-sanctum-vue-client/src/main.js
@@ -3,13 +3,18 @@ import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import useAuth from "@/composables/useAuth.js";
 
 axios.defaults.baseURL = 'http://laravel-sanctum-vue-api.test'
 axios.defaults.withCredentials = true
 axios.defaults.withXSRFToken = true
 
+const { attempt } = useAuth()
+
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+attempt().then(() => {
+    app.mount('#app')
+})
