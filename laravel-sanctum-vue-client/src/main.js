e8b3ab24abb287f5bd8c1104be9d1752147dc4d0
Alex Garrett-Smith <alex@codecourse.com>
2024-05-15 14:14:55 +0100

    Episode 06

--- laravel-sanctum-vue-client/src/main.js
+++ laravel-sanctum-vue-client/src/main.js
@@ -1,8 +1,12 @@
 import './assets/main.css'
-
+import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = 'http://laravel-sanctum-vue-api.test'
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
 
 const app = createApp(App)
 
