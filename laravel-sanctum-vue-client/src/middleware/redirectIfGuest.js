0d205f18d725c0b634f8470e74a82cbc0a7bc9b3
Alex Garrett-Smith <alex@codecourse.com>
2024-05-16 10:56:40 +0100

    Episode 19

---
+++ laravel-sanctum-vue-client/src/middleware/redirectIfGuest.js
@@ -0,0 +1,13 @@
+import useAuth from "@/composables/useAuth.js";
+
+const { authenticated } = useAuth()
+
+export default function redirectIfGuest({ next }) {
+    if (!authenticated.value) {
+        return next({
+            name: 'login'
+        })
+    }
+
+    return next()
+}
